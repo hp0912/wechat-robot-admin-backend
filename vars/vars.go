6a414afb43d25d0821b312e6d65ca603927e8b5b
hp0912 <809211365@qq.com>
2025-06-16 21:03:27 +0800

    feat: 公众号链接环境变量

--- vars/vars.go
+++ vars/vars.go
@@ -11,6 +11,7 @@ var DockerComposeCmd string
 
 var WeChatServerAddress string // 登录管理员后台的微信鉴权服务器地址 公众号扫码登录
 var WeChatServerToken string
+var WeChatOfficialAccountAuthURL string // 微信公众号授权地址
 
 var SessionSecret string
 
