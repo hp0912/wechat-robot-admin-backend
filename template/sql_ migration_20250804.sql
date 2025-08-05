45af7280412748532b33b30e8b320adf6459a423
hp0912 <809211365@qq.com>
2025-08-05 22:57:00 +0800

    feat: 建表语句字段类型错误

--- template/sql_ migration_20250804.sql
+++ template/sql_ migration_20250804.sql
@@ -15,3 +15,7 @@ alter table chat_room_settings
 
 alter table chat_room_settings
     add image_recognition_model varchar(100) default '' null comment '图像识别模型名称' after chat_model;
+
+# 修复历史bug
+alter table global_settings
+    modify pat_text varchar(255) default '' null comment '拍一拍文本';
