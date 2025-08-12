61247a3f139c2ae159786df6d304ca170e782173
hp0912 <809211365@qq.com>
2025-08-12 16:08:35 +0800

    feat: api token

--- model/user.go
+++ model/user.go
@@ -6,6 +6,7 @@ type User struct {
 	ID          int64          `gorm:"primarykey" json:"id"`
 	WeChatId    string         `json:"wechat_id" gorm:"column:wechat_id;index"`
 	DisplayName string         `json:"display_name" gorm:"column:display_name;" validate:"max=20"`
+	ApiToken    string         `json:"api_token" gorm:"column:api_token;type:varchar(128);index"`
 	Role        int            `json:"role" gorm:"type:int;default:1"`   // admin, common
 	Status      int            `json:"status" gorm:"type:int;default:1"` // enabled, disabled
 	AvatarUrl   string         `json:"avatar_url" gorm:"type:varchar(500);column:avatar_url;default:''"`
