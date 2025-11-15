2b206537dadb1fa2544c2ac8cb2f9f2b608d80fd
hp0912 <809211365@qq.com>
2025-11-15 10:33:33 +0800

    feat: webhook

--- dto/system_settings.go
+++ dto/system_settings.go
@@ -1,16 +1,18 @@
 package dto
 
 type SystemSettingsRequest struct {
-	ID                         int64   `json:"id"`
-	SystemSettingsID           int64   `json:"system_settings_id"`
-	APITokenEnabled            *bool   `json:"api_token_enabled"`
-	OfflineNotificationEnabled *bool   `json:"offline_notification_enabled"`
-	NotificationType           string  `json:"notification_type"`
-	PushPlusURL                *string `json:"push_plus_url"`
-	PushPlusToken              *string `json:"push_plus_token"`
-	AutoVerifyUser             *bool   `json:"auto_verify_user"`
-	VerifyUserDelay            int     `json:"verify_user_delay"`
-	AutoChatroomInvite         *bool   `json:"auto_chatroom_invite"`
+	ID                         int64          `json:"id"`
+	SystemSettingsID           int64          `json:"system_settings_id"`
+	WebhookURL                 *string        `json:"webhook_url"`
+	WebhookHeaders             map[string]any `json:"webhook_headers"`
+	APITokenEnabled            *bool          `json:"api_token_enabled"`
+	OfflineNotificationEnabled *bool          `json:"offline_notification_enabled"`
+	NotificationType           string         `json:"notification_type"`
+	PushPlusURL                *string        `json:"push_plus_url"`
+	PushPlusToken              *string        `json:"push_plus_token"`
+	AutoVerifyUser             *bool          `json:"auto_verify_user"`
+	VerifyUserDelay            int            `json:"verify_user_delay"`
+	AutoChatroomInvite         *bool          `json:"auto_chatroom_invite"`
 }
 
 type SystemSettings struct {
